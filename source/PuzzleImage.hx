d8794e6b7ffd0f913767c3dc42d7af0c30161a6e
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-08-12 23:30:28 +0100

    Comment out redundant fields

--- source/PuzzleImage.hx
+++ source/PuzzleImage.hx
@@ -6,10 +6,9 @@ import openfl.utils.IAssetCache;
 
 class PuzzleImage
 {
-	public var Path(default, null):String;
-	public var Width(default, null):Int;
-	public var Height(default, null):Int;
-
+	/*public var Path(default, null):String;
+		public var Width(default, null):Int;
+		public var Height(default, null):Int; */
 	public static var Images(default, null):haxe.ds.Map<String, Int> = ["assets/images/PuzzleImages/placa7.png" => 647];
 	/*public function new(path:String, width:Int, height:Int)
 		{
